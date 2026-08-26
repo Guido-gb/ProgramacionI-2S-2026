8abecebd45fe34efcac5e78f18ef90ba6cf14373
unknown <guidobaldini.gmail.com>
2026-08-26 11:06:30 -0300

    ejercicios de recursion

--- Ejercicios_Practica/src/parciales/Recursion.java
+++ Ejercicios_Practica/src/parciales/Recursion.java
@@ -3,8 +3,8 @@ package parciales;
 
 public class Recursion {
 	public static void main(String[] args) {
-		String prueba = "bcd";
-		System.out.println(repetirLetras(prueba));
+		String prueba = "xax";
+		System.out.println(eliminarLetraEntreIguales(prueba,'z'));
 	}
 	
 	public static String espejar(String s) {
@@ -37,4 +37,57 @@ public class Recursion {
 		vocales += vocales.toUpperCase();
 		return vocales.indexOf(caracter) != -1;
 	}
+	
+	public static String reemplazarCaracter(String s,char original, char nuevo) {
+		if(s.isEmpty()) {
+			return s;
+		}
+		if(s.charAt(0) == original) {
+			return nuevo 
+					+ reemplazarCaracter(s.substring(1), original, nuevo);
+		}
+		return s.charAt(0) 
+				+ reemplazarCaracter(s.substring(1), original, nuevo);
+		
+	}
+	
+	public static String duplicarDesde(String s, int pos) {
+		if(s.isEmpty()) {
+			return s;
+		}
+		if(pos == 0) {
+			return "" + s.charAt(0) + s.charAt(0) 
+			+ duplicarDesde(s.substring(1), pos);
+		}
+		return s.charAt(0) + duplicarDesde(s.substring(1), pos-1);
+	}
+	
+	public static boolean tieneMasDeNVocales(String s, int n) {
+		if(s.isEmpty()) {
+			return false;
+		}
+		if(n == 0 && esVocal(s.charAt(0))) {
+			return true;
+		}
+		if(esVocal(s.charAt(0))) {
+			return tieneMasDeNVocales(s.substring(1), n-1);
+		}
+		return tieneMasDeNVocales(s.substring(1), n);
+	}
+	
+	public static String eliminarLetraEntreIguales(String s, char a) {
+		if(s.length() < 3) {
+			return s;
+		}
+		if(s.charAt(1) == a && s.charAt(0) == s.charAt(2)) {
+			return s.charAt(0) 
+					+ eliminarLetraEntreIguales(s.substring(2), a);
+		}
+		return s.charAt(0) 
+				+ eliminarLetraEntreIguales(s.substring(1), a);
+	}
 }
+
+
+
+
