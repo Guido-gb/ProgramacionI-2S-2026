330f6bf550930e2697df61d3948da4aeec20d83d
unknown <guidobaldini.gb@gmail.com>
2026-09-16 11:21:03 -0300

    dibujador y ejercicios

--- Ejercicios_Practica/src/objetos__ejercicio/Comision.java
+++ Ejercicios_Practica/src/objetos__ejercicio/Comision.java
@@ -6,4 +6,38 @@ public class Comision {
 	Docente[] docentes;
 	Estudiante[] inscriptos;
 	int[] calificaciones;
+	
+	public boolean esta(Estudiante e) {
+		for(Estudiante es:this.inscriptos) {
+			if(es.equals(e)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean esta(Docente d) {
+		for(Docente doc:this.docentes) {
+			if(d.equals(doc)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int mejores() {
+		int mejorNota= this.calificaciones[0];
+		for(int pos=0;pos < this.calificaciones.length;pos++) {
+			if(mejorNota < this.calificaciones[pos]) {
+				mejorNota = this.calificaciones[pos];
+			}
+		}
+		int cantAlumnos=0;
+		for(int pos=0;pos < this.calificaciones.length;pos++) {
+			if(mejorNota == this.calificaciones[pos]) {
+				cantAlumnos++;
+			}
+		}
+		return cantAlumnos;
+	}
 }
--- Ejercicios_Practica/src/objetos__ejercicio/UNGS.java
+++ Ejercicios_Practica/src/objetos__ejercicio/UNGS.java
@@ -11,4 +11,76 @@ public class UNGS {
 		}
 		return false;
 	}
+	
+	boolean suficientesDocentes() {
+		for(Comision c:this.comisiones) {
+			if(c.inscriptos.length > 20 * c.docentes.length) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	Estudiante elMasEstudioso() {
+		Estudiante supremo = this.comisiones[0].inscriptos[0];
+		for(Comision c:this.comisiones) {
+			for(Estudiante e:c.inscriptos) {
+				if(cantAprobadas(e) > cantAprobadas(supremo)) {
+					supremo = e;
+				}
+			}
+		}
+		return supremo;
+	}
+
+	public int cantAprobadas(Estudiante e) {
+		int cont =0;
+		for(Comision c:this.comisiones) {
+			for(int pos=0; pos < c.inscriptos.length;pos++) {
+				if(c.inscriptos[pos].equals(e) 
+						&& c.calificaciones[pos] >= 4) {
+					cont++;
+				}
+			}
+		}
+		return cont;
+	}
+	
+	int losMejores() {
+		int cont=0;
+		for(Comision c: this.comisiones) {
+			cont = cont+c.mejores();
+		}
+		return cont;
+	}
+	
+	int alumnosDe(Docente d) {
+		int cont=0;
+		for(Comision c:this.comisiones) {
+			if(c.esta(d)) {
+				cont+=c.inscriptos.length;
+			}
+		}
+		return cont;
+	}
+	
+	int unicaComision() {
+		int cont=0;
+		for(Comision c:this.comisiones) {
+			if(esUnica(c.materia)) {
+				cont++;
+			}
+		}
+		return cont;
+	}
+
+	public  boolean esUnica(String mat) {
+		int cont=0;
+		for(Comision c:this.comisiones) {
+			if(c.materia.equals(mat)) {
+				cont++;
+			}
+		}
+		return cont == 1;
+	}
 }
